167721fa68ca160ac54e99e521762bc52c46133f
徐浩东 <192243498@qq.com>
2022-03-06 20:05:17 +0800

    second commit !!!

--- src/main/java/com/xhd/App.java
+++ src/main/java/com/xhd/App.java
@@ -12,5 +12,7 @@ public class App
         System.out.println("hello git!!!!");
         System.out.println( "Hello World!" );
         System.out.println("11111111111111");
+        System.out.println("2222222222222222");
+
     }
 }
