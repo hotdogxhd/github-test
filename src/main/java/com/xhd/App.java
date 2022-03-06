7ad20676a485f19b0a0dc89c566dcaa828c39434
徐浩东 <192243498@qq.com>
2022-03-06 20:02:36 +0800

    first commit !!!

---
+++ src/main/java/com/xhd/App.java
@@ -0,0 +1,15 @@
+package com.xhd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println("hello git!!!!");
+        System.out.println( "Hello World!" );
+    }
+}
